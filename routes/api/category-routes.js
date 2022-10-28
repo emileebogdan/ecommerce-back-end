da9b4d46ca626d2fce92ec4d81af770e58fb8734
emileebogdan <emileebogdan@gmail.com>
2022-10-28 15:45:46 -0600

    routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,16 +1,31 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [Product],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id)
+    
+    if(!categoryData) {
+      res.status(404).json({message: "No category data with this id!"})
+      return;
+    }
+
+    res.status(200).json(categoryData)
+
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
