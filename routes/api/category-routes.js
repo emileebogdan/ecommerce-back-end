e5129c139e494db19d4e91365e3bff5526dcf64d
emileebogdan <emileebogdan@gmail.com>
2022-10-28 15:56:17 -0600

    post routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,7 +14,9 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const categoryData = await Category.findByPk(req.params.id)
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [Product],
+    });
     
     if(!categoryData) {
       res.status(404).json({message: "No category data with this id!"})
@@ -23,13 +25,20 @@ router.get('/:id', async (req, res) => {
 
     res.status(200).json(categoryData)
 
-  } catch (error) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = Category.create(req.body)
+    res.status(200).json(categoryData)
+
+  } catch (err) {
+    res.status(500).json(err);
+
+  }
 });
 
 router.put('/:id', (req, res) => {
@@ -41,3 +50,10 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+
+
+// activity 7 bookRoutes for the .put route and .delete route
+
+// finish the routes 
+
+// rewrite category routes in product routes and tag routes 
